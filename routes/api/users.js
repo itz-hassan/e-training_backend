75cb945d16451f3442593fe65160438bf8bed793
Hassan Usman <husmanjah@gmail.com>
2022-04-03 14:36:53 +0100

    intergrated payment system

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,16 +5,35 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 //Load user model for email exist checking
 const _ = require("lodash");
-const { User, validate } = require("../../models/user.model");
+const { User, validate, validatePay } = require("../../models/user.model");
+const { initializePay, verifyPayment } = require("./paystack");
 
 // @route  GET   api/users/
 // @desc   Register users route
 // @access Public
 
+router.get("/verifyPay", async (req, res) => {
+  if (!req.query.reference) return res.status(400).send("no refernce");
+  const result = await verifyPayment(req.query.reference);
+  res.send(result);
+});
+
+router.post("/pay", async (req, res) => {
+  console.log("Paying...", req.body);
+  const { error } = validatePay(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const result = await initializePay(req.body);
+  if (result.status == undefined)
+    return res
+      .status(500)
+      .send("Network Error. make sure you are connected to internet");
+  res.send(result.data);
+});
+
 router.post("/", async (req, res) => {
+  console.log(req.body);
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-
   const user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("Email already exists");
 
@@ -26,10 +45,12 @@ router.post("/", async (req, res) => {
     role: req.body.role,
     levelOfEduc: req.body.levelOfEduc,
     phone: req.body.phone,
+    gender: req.body.gender,
     dob: req.body.dob,
     name: `${req.body.last_name} ${req.body.first_name}`,
     address: req.body.address,
     registration: req.body.registration,
+    specialization: req.body.specialization,
   });
 
   const salt = await bcrypt.genSalt(10);
@@ -38,62 +59,6 @@ router.post("/", async (req, res) => {
   await newUser.save();
   const token = newUser.generateAuthToken();
   res.header("x-auth-token", token).send({ token });
-});
-
-// @route  GET   api/users/login
-// @desc   Login users route => returning jwt token
-// @access Public
-
-router.post("/users/login", (req, res) => {
-  const { errors, isValid } = validateLoginInput(req.body);
-
-  // Check Validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  const email = req.body.email;
-  const password = req.body.password;
-
-  //Find user by email
-  User.findOne({ email }).then((user) => {
-    if (!user) {
-      errors.email = "User not found";
-      return res.status(404).json(errors);
-    }
-    //check password
-    bcrypt.compare(password, user.password).then((isMatch) => {
-      if (isMatch) {
-        //User Match
-
-        //Create jt payload
-        const payload = {
-          id: user.id,
-          first_name: user.first_name,
-          last_name: user.last_name,
-          avatar: user.avatar,
-          role: user.role,
-        };
-        //Sign token
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          { expiresIn: 3600 },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token,
-              first_name: user.first_name,
-              last_name: user.last_name,
-            });
-          }
-        );
-      } else {
-        errors.password = "Password incorrect";
-        return res.status(400).json(errors);
-      }
-    });
-  });
 });
 
 module.exports = router;
